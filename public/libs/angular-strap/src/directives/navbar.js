8c49138f2e6e12a650c3d2d2b2352b922d74b28c
yahyaKacem <fuj.tyoli@gmail.com>
2013-03-11 17:17:28 +0100

    last commit

--- public/libs/angular-strap/src/directives/navbar.js
+++ public/libs/angular-strap/src/directives/navbar.js
@@ -1,9 +1,6 @@
-
 angular.module('$strap.directives')
-
 .directive('bsNavbar', ['$location', function($location) {
   'use strict';
-
   return {
     restrict: 'A',
     link: function postLink($scope, element, attrs, controller) {
@@ -12,19 +9,20 @@ angular.module('$strap.directives')
         return $location.path();
       }, function(newValue, oldValue) {
 
+
         element.find('li[data-match-route]').each(function(k, li) {
           var $li = angular.element(li),
             // data('match-rout') does not work with dynamic attributes
             pattern = $li.attr('data-match-route'),
             regexp = new RegExp('^' + pattern + '$', ["i"]);
-
           if(regexp.test(newValue)) {
             $li.addClass('active');
           } else {
             $li.removeClass('active');
           }
-
         });
+
+
       });
     }
   };
